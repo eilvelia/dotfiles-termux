1f2c18496f3614fe2c2ca2734ed636d260a3501a
Bannerets <save14@protonmail.com>
2017-11-25 19:43:05 +0200

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+### Installation
+
+##### Requirements
+
+- Android
+- Termux
+
+```sh
+pkg update
+pkg install git
+git clone https://github.com/Bannerets/dotfiles-termux.git dotfiles
+cd dotfiles
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg update
+pkg upgrade
+
+pkg install coreutils
+pkg install vim
+pkg install nano
+
+pkg install git
+pkg install openssh
+pkg install gnupg
+
+pkg install screenfetch
+pkg install neofetch
+
+pkg install nodejs
+pkg install python
+pkg install ruby
+
+pkg install nmap
+
+### sudo
+
+pkg install ncurses-utils
+
+cp ./sudo /data/data/com.termux/files/usr/bin/sudo
+chmod 700 /data/data/com.termux/files/usr/bin/sudo
+
+### ---
